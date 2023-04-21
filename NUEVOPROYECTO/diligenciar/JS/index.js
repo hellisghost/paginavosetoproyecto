78ec542ba4b95cdaaefeb4ec66d0c0165a2e3e31
hellisghost <andrademahechafernando@gmail.com>
2023-04-21 16:50:46 -0500

    finalizacion

---
+++ NUEVOPROYECTO/diligenciar/JS/index.js
@@ -0,0 +1,12 @@
+
+function mamahuevo (){
+    let n1,cubo
+    n1 = document.getElementById("numero1").value
+
+
+cubo = n1*n1*n1
+document.getElementById("respuesta").innerHTML = 
+"<div> el valor es " +cubo+"</div>"
+
+}
+
---
+++ NUEVOPROYECTO/trabajo/index.js
@@ -0,0 +1,41 @@
+function descuento1(){
+    let n1
+    n1 = document.getElementById("numero1").value
+   let n2
+    n2 = document.getElementById("numero2").value
+
+    let descuento1
+    descuento1 = document.getElementById("descuento").value
+
+descuento1 = n1*n2/descuento1
+document.getElementById("respuesta").innerHTML = 
+"<div> el valor es " +n1*n2/descuento1+"</div>"
+}
+
+function descuento2(){
+    let n1
+    n1 = document.getElementById("numero1").value
+   let n2
+    n2 = document.getElementById("numero2").value
+
+    let descuento2
+    descuento2 = document.getElementById("descuento").value
+
+descuento2 = n1*n2/descuento2
+document.getElementById("respuesta").innerHTML = 
+"<div> el valor es " +n1*n2/descuento2+"</div>"
+}
+
+function descuento3(){
+    let n1
+    n1 = document.getElementById("numero1").value
+   let n2
+    n2 = document.getElementById("numero2").value
+
+    let descuento3
+    descuento3 = document.getElementById("descuento").value
+
+descuento3 = n1*n2/descuento3
+document.getElementById("respuesta").innerHTML = 
+"<div> el valor es " +n1*n2/descuento3+"</div>"
+}
