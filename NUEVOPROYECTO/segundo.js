7177cade594d383ef58754ceefbd7731cd824872
hellisghost <andrademahechafernando@gmail.com>
2023-04-21 09:27:09 -0500

    nuevos procesos

---
+++ NUEVOPROYECTO/segundo.js
@@ -0,0 +1,17 @@
+alert("BIENVENIDOS")
+
+function getfiche(){
+    let producto = document.getElementById("nom_producto").value;
+    document.getElementById("res_producto").innerText = producto
+    let product = document.getElementById("sim_producto").value;
+    document.getElementById("ros_producto").innerText = product;
+    let productor = document.getElementById("vat_producto").value;
+    document.getElementById("ris_producto").innerText = productor
+    let productore = document.getElementById("va_producto").value;
+    document.getElementById("rus_producto").innerText = productore
+    let productores = document.getElementById("mo_producto").value;
+    document.getElementById("rrs_producto").innerText = productores
+    let productoress = document.getElementById("mi_producto").value;
+    document.getElementById("rps_producto").innerText = productoress
+}
+
